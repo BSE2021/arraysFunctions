f406cd3e9f363a747c79d5d00d15c81fed01c8b7
Beula Samuel <beulathamban@gmail.com>
2021-10-11 11:02:03 +1300

    added minimum function

--- arraysFunctions.cpp
+++ arraysFunctions.cpp
@@ -4,7 +4,9 @@
 #include <iostream>
 using namespace std;
 
-void maxim(float p[],int n) {
+void minim(double p[], int n);
+
+void maxim(double p[],int n) {
 	int i;
 	float max = -10000;
 
@@ -18,14 +20,26 @@ void maxim(float p[],int n) {
 
 int main()
 {
-	float price[] = { 3.5, 2.72, 3.3, 1.55 };
+	double price[] = { 3.5, 2.72, 3.3, 1.55 };
 	int i;
-	int length = sizeof(price) / sizeof(float);
+	int length = sizeof(price) / sizeof(double);
 	cout << "\nSize of the array= " << length << endl;
 	for (i = 0; i < length; i++) {
 		cout << "\nValue " << i << " = " << price[i]<<endl;
 		
 	}
 	maxim(price, length);
+	minim(price, length);
 	return 0;
+}
+void minim(double p[], int n) {
+	int i;
+	float min = 1000;
+
+	for (i = 0; i < n; i++) {
+		if (p[i] < min) {
+			min = p[i];
+		}
+	}
+	cout << "\nThe lowest price =  " << min << endl;
 }
